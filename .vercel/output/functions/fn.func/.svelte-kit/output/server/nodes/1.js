6b1c30eabce12c64cc444a7f6d352ea4e1a4da18
puppy <h.luanph@proton.me>
2023-09-13 21:41:22 +0200

    add vercel support

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.ff04975d.js","_app/immutable/chunks/scheduler.387d4020.js","_app/immutable/chunks/index.c0f316b9.js","_app/immutable/chunks/singletons.0d0472fe.js","_app/immutable/chunks/index.f7adf726.js"];
+export const imports = ["_app/immutable/nodes/1.38887201.js","_app/immutable/chunks/scheduler.387d4020.js","_app/immutable/chunks/index.c0f316b9.js","_app/immutable/chunks/singletons.078fa873.js","_app/immutable/chunks/index.f7adf726.js"];
 export const stylesheets = ["_app/immutable/assets/1.7aad9d3a.css"];
 export const fonts = [];
--- .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["dog_hover.gif","dog_idle.gif","error.gif","oh-my-gaah.gif"]),
 	mimeTypes: {".gif":"image/gif"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.9d2d424e.js","app":"_app/immutable/entry/app.63706184.js","imports":["_app/immutable/entry/start.9d2d424e.js","_app/immutable/chunks/scheduler.387d4020.js","_app/immutable/chunks/singletons.0d0472fe.js","_app/immutable/chunks/index.f7adf726.js","_app/immutable/chunks/control.f5b05b5f.js","_app/immutable/entry/app.63706184.js","_app/immutable/chunks/preload-helper.a4192956.js","_app/immutable/chunks/scheduler.387d4020.js","_app/immutable/chunks/index.c0f316b9.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.ef84d4ce.js","app":"_app/immutable/entry/app.42b7a577.js","imports":["_app/immutable/entry/start.ef84d4ce.js","_app/immutable/chunks/scheduler.387d4020.js","_app/immutable/chunks/singletons.078fa873.js","_app/immutable/chunks/index.f7adf726.js","_app/immutable/chunks/control.f5b05b5f.js","_app/immutable/entry/app.42b7a577.js","_app/immutable/chunks/preload-helper.a4192956.js","_app/immutable/chunks/scheduler.387d4020.js","_app/immutable/chunks/index.c0f316b9.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js'))
