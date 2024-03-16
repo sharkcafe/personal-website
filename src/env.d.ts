d0e17b85560108c7ddf48fe23d3b442a0d927ddd
pluwshy <h.luanph@proton.me>
2024-03-16 23:42:13 +0100

    removed unused feature + updated readme

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,12 +2,6 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
-declare module "@pagefind/default-ui" {
-	declare class PagefindUI {
-		constructor(arg: unknown);
-	}
-}
-
 interface ImportMeta {
 	readonly env: ImportMetaEnv;
 }
